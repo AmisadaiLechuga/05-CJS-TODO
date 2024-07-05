cba5c3ec9284a89002bbe582f38b33e092a953a5
Amisadai Lechuga <amisadailechu@gmail.com>
2024-07-05 11:49:42 -0600

    Método: Crear un nuevo TODO

--- src/todos/app.js
+++ src/todos/app.js
@@ -4,6 +4,7 @@ import { renderTodos } from './use-cases';
 
 const ElementIDs = {
     TodoList: '.todo-list',
+    NewTodoInput: '#new-todo-input',
 }
 
 
@@ -31,6 +32,20 @@ export const App = (elementid) => {
 
 })();
 
+    //Refrencias HTML
+    const newDescriptionInput = document.querySelector( ElementIDs.NewTodoInput );
+
+
+    //Listeners
+    newDescriptionInput.addEventListener('keyup', ( event ) => {
+
+        if ( event.keyCode !==13 ) return;
+        if( event.target.value.trim().length ===0 ) return;
+
+        todoStore.addTodo( event.target.value );
+        displayTodos();
+        event.target.value = '';
+    });
 
 
 }
