a7cf99891c78a06587a88fa031fb5785954210cd
Amisadai Lechuga <amisadailechu@gmail.com>
2024-07-05 10:13:31 -0600

    Validaciones y optimizaciones

--- src/todos/use-cases/create-todo-html.js
+++ src/todos/use-cases/create-todo-html.js
@@ -7,9 +7,26 @@ import { Todo } from "../models/todo.model";
 export const createTodoHTML = ( todo ) =>{
     if( !todo ) throw new Error('A TODO object is required');
 
-    const html = `<h1>${ todo.description }</h1>`;
+    const { done, description, id } = todo; //desestructuracion de datos para no poner ---> todo.id, todo.description, todo.done sino solo ---> done, description and id
+    const html = `
+                    <div class="view">
+                        <input class="toggle" type="checkbox" ${ done ? 'checked' : '' }>
+                        <label>${ description }</label>
+                        <button class="destroy"></button>
+                    </div>
+                        <input class="edit" value="Create a TodoMVC template">
+                `;
+    //const html = `<h1>${ todo.description }</h1>`; cambiarlo por el codigo de arriba
     const liElement = document.createElement('li');
     liElement.innerHTML = html;
+    //liElement.setAttribute('data-id', todo.id);
+    liElement.setAttribute('data-id', id);
+
+    if (todo.done)
+    liElement.classList.add('completed');
+
+
+
     return liElement;
 
 }
--- src/todos/use-cases/render-todos.js
+++ src/todos/use-cases/render-todos.js
@@ -1,6 +1,10 @@
 import { Todo } from '../models/todo.model';
 import { createTodoHTML } from './';
 
+
+let element;
+
+
 /**
  * 
  * @param {String} elementId 
@@ -9,9 +13,13 @@ import { createTodoHTML } from './';
 
 export const renderTodos = ( elementId, todos = [] ) => {
 
-    //TODO referencia
-    const element = document.querySelector( elementId );
+    if ( !element )
+    element = document.querySelector( elementId );
 
+   if ( !elementId ) throw new Error (`Elemnt ${ elementId } not found `); 
+
+   element.innerHTML = '';
+   
     todos.forEach( todo => {
 
         element.append( createTodoHTML(todo) )
